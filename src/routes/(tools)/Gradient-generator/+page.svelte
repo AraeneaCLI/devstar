d3b4565f8190fb468e753869f40b438284416251
edmondjonathan02 <edmondjonathan1382@gmail.com>
2023-10-04 20:54:33 +0530

    Modified Gradient generator file & added ui

--- src/routes/(tools)/Gradient-generator/+page.svelte
+++ src/routes/(tools)/Gradient-generator/+page.svelte
@@ -1,23 +1,126 @@
 <script lang="ts">
-    import Intro from '$lib/Intro.svelte'
-    import { Label, Input } from 'flowbite-svelte';
-    export let data;
+  import { Label, Select } from "flowbite-svelte";
+
+  let gradientColors = ["#FF5733", "#6A82FB"];
+  let gradientType = "linear-gradient";
+  let gradientCode = "";
+  let showCopyNotification = false;
+
+  function generateGradient() {
+    gradientCode = `${gradientType}(${gradientColors.join(", ")})`;
+  }
+
+  function copyCSS() {
+    const cssText = `${gradientType}(${gradientColors.join(", ")})`;
+    const el = document.createElement("textarea");
+    el.value = cssText;
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand("copy");
+    document.body.removeChild(el);
+    showCopyNotification = true;
+
+    // Hide the notification after a few seconds (e.g., 3 seconds)
+    setTimeout(() => {
+      showCopyNotification = false;
+    }, 3000);
+  }
+
+  function updateGradient() {
+    generateGradient();
+  }
+
+  function generateRandomGradient() {
+    gradientColors = [getRandomColor(), getRandomColor()];
+    generateGradient();
+  }
+
+  function getRandomColor() {
+    const letters = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
 </script>
 
-<Intro heading={data.meta.title} description={data.meta.description} />
+<main class="min-h-screen flex items-center justify-center">
+  <div class="w-full max-w-4xl p-8 bg-white rounded-lg shadow-md dark:bg-gray-800 flex">
+    <!-- Settings (Left Half) -->
+    <div class="w-1/2 p-4">
+      <h1 class="text-2xl font-semibold text-gray-800 dark:text-white">CSS Gradient Generator</h1>
 
+      <!-- Gradient Type Selector -->
+      <div class="mt-4">
+        <Label for="gradientType" class="text-gray-700 dark:text-gray-400">Gradient Type:</Label>
+        <Select
+          id="gradientType"
+          bind:value={gradientType}
+          class="mt-1"
+        >
+          <option value="linear-gradient">Linear Gradient</option>
+          <option value="radial-gradient">Radial Gradient</option>
+        </Select>
+      </div>
 
-<button id="dropdownHoverButton" data-dropdown-toggle="dropdownHover" data-dropdown-trigger="hover" class="text-white bg-white hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">Dropdown hover <svg class="w-2.5 h-2.5 ml-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
-  </svg>
-</button>
+      <!-- Color Pickers -->
+      <div class="mt-4">
+        <Label class="text-gray-700 dark:text-gray-400">Gradient Colors:</Label>
+        <div class="flex space-x-2">
+          {#each gradientColors as color, index}
+            <input
+              type="color"
+              bind:value={gradientColors[index]}
+              class="mt-1"
+              on:input={updateGradient}
+            />
+          {/each}
+        </div>
+      </div>
 
-<div id="dropdownHover" class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
-    
-</div>
+      <!-- Button Container -->
+      <div class="mt-4 flex items-center">
+        <!-- Generate and Random Buttons -->
+        
+          <button
+            type="button"
+            class="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            on:click={generateGradient}
+          >
+            Generate Gradient
+          </button>
+          <button
+            type="button"
+            class="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center ml-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            on:click={generateRandomGradient}
+          >
+            Random
+          </button>
+        </div>
 
+        <!-- Copy CSS Button -->
+        <button
+          type="button"
+          class="text-white bg-green-500 hover:bg-green-600 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800 mt-2"
+          on:click={copyCSS}
+        >
+          Copy CSS
+        </button>
 
-<button type="button" class="py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-full border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Random</button>
-<button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Copy CSS</button>
+        <!-- Notification (Initially hidden) -->
+        {#if showCopyNotification}
+          <div class="text-sm text-green-600 mt-2">CSS Copied</div>
+        {/if}
+      
+    </div>
 
-
+    <!-- Gradient Display (Right Half) -->
+    <div class="w-1/2 p-4">
+      <div
+        class="w-full h-64"
+        style="background-image: {gradientCode};"
+      ></div>
+    </div>
+  </div>
+</main>
